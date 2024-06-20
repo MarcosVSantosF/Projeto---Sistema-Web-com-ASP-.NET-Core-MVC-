545b4e08cf0b77a6d2f82707853cfc8f2ce2b018
Marcos Vinicius <marcosvsantosf@gmail.com>
2024-06-20 18:43:55 -0300

    Implementing grouping search

--- SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/Services/DepartmentService.cs
@@ -13,15 +13,6 @@ namespace SalesWebMVC.Services
         {
             _context = context;
         }
-
-        /* Chamada síncrona: 
-          public List<Department> FindAll()
-         {
-             return _context.Department.OrderBy(x => x.Name).ToList();
-         }*/
-
-
-        //Chamada Assíncrona incluindo Task(async e await)
         public async Task<List<Department>> FindAllAsync()
         {
             return await _context.Department.OrderBy(x => x.Name).ToListAsync();
