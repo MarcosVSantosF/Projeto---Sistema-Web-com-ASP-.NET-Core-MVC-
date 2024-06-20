731fa83afa6fc62fd5bd20f7bce3cad0c2ce627c
Marcos Vinicius <marcosvsantosf@gmail.com>
2024-06-20 18:12:49 -0300

    Implementing simple search

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -29,7 +29,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options => options.UseMySql(co
 builder.Services.AddScoped<SeedingService>();
 builder.Services.AddScoped<SellerService>();
 builder.Services.AddScoped<DepartmentService>();
-/*builder.Services.AddScoped<SalesRecordService>();*/
+builder.Services.AddScoped<SalesRecordService>();
 
 var app = builder.Build();
 
